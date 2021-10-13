c22e6e5c6a6afaa55af5a82f812a57ef7ec75df9
TaeGyun <taekuen98@naver.com>
2021-10-13 21:08:24 +0900

    21.10.13

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){   //새로 생성하는 객체
+            em.persist(item);
+        }else{  //이미 db에 등록되어 있는것을 가져온다 약간 update 느낌
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select from Item i", Item.class)
+                .getResultList();
+    }
+
+}
